1d1570a97556418dd3620c66a777574ae0705e9d
JuJarazo <julianjarazo94@gmail.com>
2022-07-28 19:00:15 -0300

    Start tailwind theming

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      white: "#FFFFFF",
+      midnight: '#0E273C',
+      purple: '#4A306D',
+      'light-purple': '#D6D3F0',
+      salmon: '#CC5A71',
+      green: '#468189'
+    },
     extend: {},
   },
   plugins: [],
