5eac452edbcd9b08d8a536b997b6657811989c24
JuJarazo <julianjarazo94@gmail.com>
2022-08-28 11:35:53 -0300

    Clean index

--- src/layout/MainLayout.tsx
+++ src/layout/MainLayout.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from 'react';
 
 export const MainLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <main className="container mx-auto flex flex-col items-center h-screen p-4">
+      {children}
+    </main>
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,48 +33,24 @@ const Home: NextPage = () => {
 
   return (
     <>
-      <main className="container mx-auto flex flex-col items-center h-screen p-4">
-        <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold">
-          Sections
-        </h1>
-        <div className="flex items-center">
-          {dummyCards.map((card) => (
-            <MainCard
-              key={card.title}
-              title={card.title}
-              description={card.description}
-              categoryDescription={card.category.description}
-              typeDescription={card.type.description}
-            />
-          ))}
-        </div>
-        <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
-          {/* {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>} */}
-        </div>
-      </main>
+      <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold">
+        Sections
+      </h1>
+      <div className="flex items-center">
+        {dummyCards.map((card) => (
+          <MainCard
+            key={card.title}
+            title={card.title}
+            description={card.description}
+            categoryDescription={card.category.description}
+            typeDescription={card.type.description}
+          />
+        ))}
+      </div>
+      <div className="pt-6 text-2xl text-blue-500 flex justify-center items-center w-full">
+        {/* {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>} */}
+      </div>
     </>
   );
 };
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm underline text-green decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
-  );
-};
-
 export default Home;
